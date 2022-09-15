6871ec9b4ad9327b574ea00cd4fdd022c0734c67
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-09-15 15:35:44 +0300

    task 2

---
+++ src/js/02-video.js
@@ -0,0 +1,38 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timeKey = "videoplayer-current-time";
+
+function durationToSave({ seconds }) {
+
+  localStorage.setItem(timeKey, seconds);
+}
+window.addEventListener('load', newStart);
+
+player.on('timeupdate', Throttle(durationToSave, 1000));
+
+function newStart() {
+  if (!localStorage.getItem(timeKey)) return;
+
+  const currentVideoTime = localStorage.getItem(timeKey);
+
+  player
+    .setCurrentTime(currentVideoTime)
+    .then(function() {
+      return player.play();
+    // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+}
